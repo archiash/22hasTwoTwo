2b2d8afcc0c3ab69844a9725b175587b30a3142f
archiash <46347714+archiash@users.noreply.github.com>
2024-03-06 14:57:52 +0700

    #

--- average.cpp
+++ average.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+
+int main(int argc, char *argv[]){
+    if(argc == 1){
+        std::cout << "Please input numbers to find average.";
+        return 0;
+    }
+
+    double sum = 0;
+    for(int i = 1; i < argc; i++){
+        sum += atof(argv[i]);
+    }
+    std::cout << "---------------------------------\n";
+    std::cout << "Average of "<<argc-1<<" numbers = " << sum / (argc - 1);
+    std::cout << "\n---------------------------------";
+}
 
